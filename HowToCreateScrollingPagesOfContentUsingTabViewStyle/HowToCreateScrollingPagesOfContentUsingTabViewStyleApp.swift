b0654633acd0e9e5e27df3294b13aa5ef7293679
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-07-04 13:38:31 +0300

    Initial Commit

---
+++ HowToCreateScrollingPagesOfContentUsingTabViewStyle/HowToCreateScrollingPagesOfContentUsingTabViewStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateScrollingPagesOfContentUsingTabViewStyleApp.swift
+//  HowToCreateScrollingPagesOfContentUsingTabViewStyle
+//
+//  Created by Ramill Ibragimov on 04.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateScrollingPagesOfContentUsingTabViewStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
